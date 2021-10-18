9e915c82dfee5a80135d2bd419987a70e52f5d15
Anay Wadhera <awadhera@berkeley.edu>
2021-10-18 12:55:58 -0700

    initial calculator classes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class Calculator extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      viewText: ""
+    }
+  }
+  handleClick() {
+    console.log("click")
+    this.setState({
+      viewText: this.state.viewText + ""
+    })
+  }
+  render() {
+    const calcView = <CalculatorView textShown={this.state.viewText}/>
+    return <div>
+      {calcView}
+      <CalculatorKeypad calcView={this.calcView} />
     </div>
-  );
+  }
 }
 
-export default App;
+class CalculatorView extends React.Component {
+  render() {
+    return <div className="calc-view">
+          {this.props.textShown}
+        </div>
+  }
+}
+
+class CalculatorKeypad extends React.Component {
+  constructor(props) {
+    //parent.handleClick()
+    super(props)
+    this.state = {
+      calcView: props.calcView
+    }
+  }
+  renderSquare(i) {
+    return <CalculatorButton value={i} calcView={this.state.calcView}/>
+  }
+  render() {
+    return <div>
+        <div className="calculator-row">
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+          {this.renderSquare(3)}
+          {this.renderSquare('+')}
+        </div>
+        <div className="calculator-row">
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+          {this.renderSquare(6)}
+          {this.renderSquare('-')}
+        </div>
+        <div className="calculator-row">
+          {this.renderSquare(7)}
+          {this.renderSquare(7)}
+          {this.renderSquare(9)}
+          {this.renderSquare('*')}
+        </div>
+        <div className="calculator-row">
+          {this.renderSquare('')}
+          {this.renderSquare(0)}
+          {this.renderSquare('')}
+          {this.renderSquare('/')}
+        </div>
+      </div>
+  }
+
+}
+
+class CalculatorButton extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: props.value,
+      calculatorView: props.calcView
+    }
+    // this.state.calculatorView.setState = this.state.calculatorView.setState.bind(this.)
+  }
+  render() {
+    return <button>
+      {this.state.value}
+    </button>
+  }
+}
