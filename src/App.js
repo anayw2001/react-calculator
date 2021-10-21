1a70d99b453b4d3e86286b0ca3b47e1c771175a7
Anay Wadhera <awadhera@berkeley.edu>
2021-10-20 23:33:47 -0700

    make the calculator actually work

--- src/App.js
+++ src/App.js
@@ -1,90 +1,130 @@
 import './App.css';
 import React from 'react';
 
+const CalculatorOperations = {
+  '/': (prevValue, nextValue) => prevValue / nextValue,
+  '*': (prevValue, nextValue) => prevValue * nextValue,
+  '+': (prevValue, nextValue) => prevValue + nextValue,
+  '-': (prevValue, nextValue) => prevValue - nextValue,
+  '=': (prevValue, nextValue) => nextValue
+}
+
 export default class Calculator extends React.Component {
   constructor() {
     super()
     this.state = {
-      viewText: ""
+      viewText: "",
+      prevValue: null,
+      operated: false,
+      operator: ""
     }
+    this.inputDigit = this.inputDigit.bind(this)
+    this.eval = this.eval.bind(this)
+    this.clearAll = this.clearAll.bind(this)
+    this.clearDisplay = this.clearDisplay.bind(this)
   }
-  handleClick() {
-    console.log("click")
+
+  clearDisplay() {
     this.setState({
-      viewText: this.state.viewText + ""
+      viewText: "0"
     })
   }
-  render() {
-    const calcView = <CalculatorView textShown={this.state.viewText}/>
-    return <div>
-      {calcView}
-      <CalculatorKeypad calcView={this.calcView} />
-    </div>
-  }
-}
 
-class CalculatorView extends React.Component {
-  render() {
-    return <div className="calc-view">
-          {this.props.textShown}
-        </div>
+  clearAll() {
+    this.setState({
+      viewText: "0",
+      prevValue: null,
+      operated: false,
+      operator: ""
+    })
   }
-}
 
-class CalculatorKeypad extends React.Component {
-  constructor(props) {
-    //parent.handleClick()
-    super(props)
-    this.state = {
-      calcView: props.calcView
+  eval(nextOperator) {
+    const { viewText, prevValue, operator } = this.state
+    const inputValue = parseFloat(viewText)
+    console.log(prevValue)
+    console.log(operator)
+    if (prevValue == null) {
+      this.setState({
+        prevValue: inputValue,
+        viewText: "0"
+      })
+    } else if (operator) {
+      const currentValue = prevValue || 0
+      const newValue = CalculatorOperations[operator](currentValue, inputValue)
+      
+      this.setState({
+        viewText: String(newValue),
+        prevValue: newValue,
+        operator: nextOperator
+      })
+    }
+    this.setState({
+      operated: false,
+      operator: nextOperator
+    })
+  }
+
+  inputDigit(i) {
+    const viewText = this.state
+    console.log("click")
+    if (this.state.operated) {
+      this.setState({
+        viewText: viewText === '0' ? String(i) : viewText + i
+      })
+    } else {
+      this.setState({
+        viewText: String(i),
+        operated: false
+      })
     }
   }
+
   renderSquare(i) {
-    return <CalculatorButton value={i} calcView={this.state.calcView}/>
+    const className = "key-" + String(i)
+    return <CalculatorButton class={className} value={i} onPress={() => this.inputDigit(i)}/>
+  }
+  renderFunction(fn) {
+    const className = "fn-" + fn
+    return <CalculatorButton class={className} value={fn} onPress={() => this.eval(fn)}/>
   }
   render() {
     return <div>
-        <div className="calculator-row">
+      {this.state.viewText}
+      <div className="calculator-row">
           {this.renderSquare(1)}
           {this.renderSquare(2)}
           {this.renderSquare(3)}
-          {this.renderSquare('+')}
+          {this.renderFunction('+')}
         </div>
         <div className="calculator-row">
           {this.renderSquare(4)}
           {this.renderSquare(5)}
           {this.renderSquare(6)}
-          {this.renderSquare('-')}
+          {this.renderFunction('-')}
         </div>
         <div className="calculator-row">
           {this.renderSquare(7)}
-          {this.renderSquare(7)}
+          {this.renderSquare(8)}
           {this.renderSquare(9)}
-          {this.renderSquare('*')}
+          {this.renderFunction('*')}
         </div>
-        <div className="calculator-row">
-          {this.renderSquare('')}
+        <div className="calculator-row-zero">
           {this.renderSquare(0)}
-          {this.renderSquare('')}
-          {this.renderSquare('/')}
+          {this.renderSquare('.')}
+          {this.renderFunction('/')}
+          {this.renderFunction('=')}
+          <CalculatorButton class="clear-all" value="AC" onPress={() => this.clearAll()}/>
+          <CalculatorButton class="clear" value="C" onPress={() => this.clearDisplay()}/>
         </div>
-      </div>
+    </div>
   }
-
 }
 
 class CalculatorButton extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      value: props.value,
-      calculatorView: props.calcView
-    }
-    // this.state.calculatorView.setState = this.state.calculatorView.setState.bind(this.)
-  }
   render() {
-    return <button>
-      {this.state.value}
+    return <button onClick={this.props.onPress}>
+      {this.props.value}
     </button>
   }
 }
